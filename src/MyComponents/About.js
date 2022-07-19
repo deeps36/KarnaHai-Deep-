e4d9d47771d22fad14c005cdfa438264a6b6667c
Deep Panchal <deep@fes.org.in>
2022-07-19 15:54:19 +0530

    Upgrade

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This Todo's App Develeoped on React Js Platform By Deep Panchal!!
+            <p>You can use this app for personal use, where you can add list of works to do. after completing you can delete entry</p>
+        </div>
+    )
+}
